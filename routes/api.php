41bee400576c2ced985ee2ca8a67f2d3591a6068
meblo98 <lomouhamedelbachir@gmail.com>
2024-08-04 13:32:50 +0000

    gestion du csrf

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,14 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
 
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
 Route::apiResource('articles', ArticleController::class);
-// Route::get('/articles/{article}/comments', [ArticleController::class, 'getComments']);
-// Route::post('/articles/{article}/comments', [ArticleController::class, 'createComment']);
-// Route::delete('/articles/{article}/comments/{comment}', [ArticleController::class, 'delete']);
-// Route::get('/articles/{article}/likes', [ArticleController::class, 'getLikes']);
-// Route::post('/articles/{article}/likes', [ArticleController::class, 'createLike']);
-// Route::delete('/articles/{article}/likes/{like}', [ArticleController::class, 'delete']);
 
